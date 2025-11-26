7e49f0603fb8d679d8a9ac6ed5c0501df2d4de16
gael <gmarcelinodocio@danielcastelao.org>
2025-11-26 13:52:02 +0100

    Añadimos metodo mostrar_tablero()

--- tablero.py
+++ tablero.py
@@ -4,3 +4,7 @@ class Tablero:
     def __init__(self, tamano):
         self.dimensiones = (tamano, tamano)
         self.casillas = [[0 for _ in range(tamano)] for _ in range(tamano)]
+
+    def mostrar_tablero(self):
+        for fila in self.casillas:
+            print(" ".join(str(celda) for celda in fila))
