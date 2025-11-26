c3c5cd51173702ce89297d20049539ab7d76afd7
gael <gmarcelinodocio@danielcastelao.org>
2025-11-26 13:51:07 +0100

    Añado atributos ( dimensiones, casillas )

--- tablero.py
+++ tablero.py
@@ -1,2 +1,6 @@
 class Tablero:
     """Clase que representa el tablero de juego."""
+
+    def __init__(self, tamano):
+        self.dimensiones = (tamano, tamano)
+        self.casillas = [[0 for _ in range(tamano)] for _ in range(tamano)]
