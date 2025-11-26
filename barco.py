b6ebb3589fb6ca717d48f62c02be6cc1838cd21f
gael <gmarcelinodocio@danielcastelao.org>
2025-11-26 13:35:37 +0100

    Añadimos atributos de barco ( nombre, longitud, golpes_recibidos)

--- barco.py
+++ barco.py
@@ -1,2 +1,7 @@
 class Barco:
     """Clase que representa un barco del juego Hundir la Flota."""
+
+    def __init__(self, nombre, longitud):
+        self.nombre = nombre
+        self.longitud = longitud
+        self.golpes_recibidos = 0
